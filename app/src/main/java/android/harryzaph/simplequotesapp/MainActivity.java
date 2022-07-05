6d324c16ba596a2cf496ffec908ce00928f24c54
harinisrees23 <58624267+harinisrees23@users.noreply.github.com>
2022-07-05 21:30:14 +0530

    QuoteApp

---
+++ app/src/main/java/android/harryzaph/simplequotesapp/MainActivity.java
@@ -0,0 +1,77 @@
+package android.harryzaph.simplequotesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    Random random = new Random();
+    TextView textQuote;
+    Button buttonQuote;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textQuote= findViewById(R.id.textViewQuote);
+        buttonQuote= findViewById(R.id.buttonNextQuote);
+
+        buttonQuote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                displayQuote();
+            }
+        });
+    }
+    public void displayQuote() {
+        int randNum = random.nextInt((12+1) - 1) + 1;
+        String randQuote = "";
+
+        switch (randNum){
+            case 1:
+                randQuote=getString(R.string.quote1);
+                break;
+            case 2:
+                randQuote=getString(R.string.quote2);
+                break;
+            case 3:
+                randQuote=getString(R.string.quote3);
+                break;
+            case 4:
+                randQuote=getString(R.string.quote4);
+                break;
+            case 5:
+                randQuote=getString(R.string.quote5);
+                break;
+            case 6:
+                randQuote=getString(R.string.quote6);
+                break;
+            case 7:
+                randQuote=getString(R.string.quote7);
+                break;
+            case 8:
+                randQuote=getString(R.string.quote8);
+                break;
+            case 9:
+                randQuote=getString(R.string.quote9);
+                break;
+            case 10:
+                randQuote=getString(R.string.quote10);
+                break;
+            case 11:
+                randQuote=getString(R.string.quote11);
+                break;
+            case 12:
+                randQuote=getString(R.string.quote12);
+                break;
+
+        }
+        textQuote.setText(randQuote);
+    }
+}
